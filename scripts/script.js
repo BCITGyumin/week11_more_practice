96303bc3a1264f6336d52b93fec44f5871269dbf
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-27 14:33:21 -0700

    setAttribute practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,82 @@
-function greet(name, time = "day") {              // function
-    console.log(`Good ${time}, ${name}!`);
-}
+// function greet(name, time = "day") {              // function
+//     console.log(`Good ${time}, ${name}!`);
+// }
 
-let greet = function(name) {        // function expression
-    console.log(`Hello ${name}!`);
-}
+// let greet = function(name) {        // function expression
+//     console.log(`Hello ${name}!`);
+// }
 
-let greet = (name) => {             //arrow function
-    console.log(`Hello ${name}!`);
-}
+// let greet = (name) => {             //arrow function
+//     console.log(`Hello ${name}!`);
+// }
 
-greet("Chris", "Night");
+// greet("Chris", "Night");
 
 
-const radiusPara = document.querySelector("#radius");
-console.log(radiusPara)
-const resultPara = document.querySelector("#result");
-console.log(resultPara)
+// const radiusPara = document.querySelector("#radius");
+// console.log(radiusPara)
+// const resultPara = document.querySelector("#result");
+// console.log(resultPara)
 
-let user_input = prompt("Enter a radius of the circle: ");
-radiusPara.textContent += user_input;
+// let user_input = prompt("Enter a radius of the circle: ");
+// radiusPara.textContent += user_input;
 
-function circleArea(input) 
-{
-    if(isNaN(input)) {
-        resultPara.textContent = "Error";
-    }
+// function circleArea(input) 
+// {
+//     if(isNaN(input)) {
+//         resultPara.textContent = "Error";
+//     }
     
-    else {
-        let radius = Math.PI * input**2; 
-        return radius;
-    }
-}
+//     else {
+//         let radius = Math.PI * input**2; 
+//         return radius;
+//     }
+// }
 
-if (circleArea(user_input) !== undefined) {
-    let raidus_result = circleArea(user_input).toFixed(2);
-    resultPara.textContent = `The area of a circle with radius ${user_input} is ${raidus_result}`;
-}
+// if (circleArea(user_input) !== undefined) {
+//     let raidus_result = circleArea(user_input).toFixed(2);
+//     resultPara.textContent = `The area of a circle with radius ${user_input} is ${raidus_result}`;
+// }
 
-const ullist = document.querySelector(".shopping");
+// const ullist = document.querySelector(".shopping");
 
-function populatelist(shoppingListArray) 
-{
-    for (let item of shoppingListArray)
-    {
-        let product = document.createElement("li");
-        product.textContent = item;
-        ullist.appendChild(product);
-    }
-}
+// function populatelist(shoppingListArray) 
+// {
+//     for (let item of shoppingListArray)
+//     {
+//         let product = document.createElement("li");
+//         product.textContent = item;
+//         ullist.appendChild(product);
+//     }
+// }
 
-let myShoppingList = ["cheese", "bread", "green pepper"];
-populatelist(myShoppingList);
+// let myShoppingList = ["cheese", "bread", "green pepper"];
+// populatelist(myShoppingList);
 
 
 
-function squareListMaker()
-{
-    ullist.classList.add("squareList");
-    ullist.classList.remove("circleList");
-}
+// function squareListMaker()
+// {
+//     ullist.classList.add("squareList");
+//     ullist.classList.remove("circleList");
+// }
 
-squareListMaker()
+// squareListMaker()
 
 
 // HTMLElement.style
 // Element.setAttribute()
+
+//week11_more_practice
+
+function updateImage()
+{
+    const image = document.querySelector("#shoppingCart");
+    image.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/263/263142.png");
+    image.setAttribute("alt", "shoppingCart");
+    image.setAttribute("width", "50");
+    image.setAttribute("height", "50");
+
+    // image.src = "https://cdn-icons-png.flaticon.com/512/263/263142.png";
+}
+updateImage();
